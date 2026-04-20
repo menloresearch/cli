78b1edb7da81bf3b614bb342abb8ca27a374b058
LazyYuuki <huy2840@gmail.com>
2026-04-20 22:22:27 +0800

    replace version subcommand with --version and -V flags

--- internal/commands/version.go
+++ internal/commands/version.go
@@ -1,31 +1,19 @@
 package commands
 
 import (
-	"fmt"
-
 	"github.com/menloresearch/cli/internal/config"
-	"github.com/spf13/cobra"
 )
 
-var versionCmd = &cobra.Command{
-	Use:   "version",
-	Short: "Print the version number",
-	Run: func(cmd *cobra.Command, args []string) {
-		cfg, err := config.Load()
-		if err != nil {
-			// If no config, use default
-			cfg = config.DefaultConfig()
-		}
+func versionString() string {
+	cfg, err := config.Load()
+	if err != nil {
+		cfg = config.DefaultConfig()
+	}
 
-		version := cfg.Version
-		if version == "" {
-			version = "dev"
-		}
+	version := cfg.Version
+	if version == "" {
+		return "dev"
+	}
 
-		fmt.Printf("menlo %s\n", version)
-	},
-}
-
-func init() {
-	rootCmd.AddCommand(versionCmd)
+	return version
 }
